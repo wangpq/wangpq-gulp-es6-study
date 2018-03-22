3fe1bfe340c9dd5c58b643a50f72eca7894202ec
Mighty Men <wpq163yx@163.com>
2018-03-22 18:10:00 +0800

    v1.0.0

--- dest/js/urlconfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";var dynsite = "http://192.168.1.251:8080";
-
-var urlconfig = {
-    fetchPhoneCharge: dynsite + "/lotteryProject/lottery/fetchPhoneCharge.action" };
-
-
-module.exports = {
-    dynsite: dynsite,
-    urlconfig: urlconfig };
